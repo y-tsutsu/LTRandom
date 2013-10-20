e72a3124733c3af4505e9e268eb8f0b603758428
y-tsutsumi <tsutsu0928@gmail.com>
2013-10-20 14:02:18 +0900

    新規作成．

---
+++ LTRandom/LTRandom.py
@@ -0,0 +1,18 @@
+﻿import codecs
+import random
+
+uf = codecs.open("LT_list.txt", "r", "shift-jis")
+list = uf.readlines()
+uf.close()
+
+length = len(list)
+if length != 0:
+    index = random.randint(0, length - 1)
+    print((u"最後は " if length == 1 else u"次は ") + list[index].replace(u"\r\n", u"") + u" です(っ´∀｀)っ")
+    del list[index]
+
+    uf = codecs.open("LT_list.txt", "w", "shift-jis")
+    uf.writelines(list)
+    uf.close()
+
+raw_input("")
