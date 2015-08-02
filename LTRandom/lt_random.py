e31501387028f330ff031755dd8385999eb2a338
tsutsu <tsutsu0928@gmail.com>
2015-08-02 11:11:58 +0900

    最近の自分のスタイルで書きなおし．

--- LTRandom/lt_random.py
+++ LTRandom/lt_random.py
@@ -2,16 +2,16 @@
 
 import random
 
-with open("LT_list.txt", "r", encoding = "shift_jis") as f:
+with open('LT_list.txt', 'r', encoding = 'shift_jis') as f:
     list = f.readlines()
 
 length = len(list)
 if length != 0:
     index = random.randint(0, length - 1)
-    print(("最後は " if length == 1 else "次は ") + list[index].replace("\n", "") + " です(っ´∀｀)っ")
+    print(('最後は ' if length == 1 else '次は ') + list[index][:-1] + ' です(っ´∀｀)っ')
     del list[index]
 
-    with open("LT_list.txt", "w", encoding = "shift_jis") as f:
+    with open('LT_list.txt', 'w', encoding = 'shift_jis') as f:
         f.writelines(list)
 
-input("")
+input('')
