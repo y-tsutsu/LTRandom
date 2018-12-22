b175c4cb7717a8fb8f8fe78ec4008534b1fdfc5e
tsutsu <tsutsu0928@gmail.com>
2018-12-22 22:58:01 +0900

    エンコーディングを変更．

--- lt_random.py
+++ lt_random.py
@@ -1,6 +1,6 @@
 import random
 
-with open('lt_list.txt', 'r', encoding='shift_jis') as f:
+with open('lt_list.txt', 'r', encoding='utf-8') as f:
     file = f.readlines()
 
 if file != []:
@@ -8,5 +8,5 @@ if file != []:
     print(('最後は ' if len(file) == 1 else '次は ') + choice[:-1] + ' です(っ´∀｀)っ')
     file.remove(choice)
 
-    with open('lt_list.txt', 'w', encoding='shift_jis') as f:
+    with open('lt_list.txt', 'w', encoding='utf-8') as f:
         f.writelines(file)
